2b0580715686f39a3141216aeb05929fc9362d65
Temirlan Khamidulla <116732626+TemirKhamidulla@users.noreply.github.com>
2023-09-15 15:41:18 +0600

    Lab1 commit

---
+++ Lab1_pp2/part2.py
@@ -0,0 +1,5 @@
+# Числа b и h можно считывать так:
+b = int(input())
+h = int(input())
+
+print(0.5 * b * h)
---
+++ Lab1_pp2/part6.py
@@ -0,0 +1,10 @@
+# Ввод числа
+number = int(input("Input: "))
+
+# Вычисление следующего и предыдущего числа
+next_number = number + 1
+previous_number = number - 1
+
+# Вывод результатов
+print("The next number for the number", number, "is", next_number,".")
+print("The previous number for the number", number, "is", previous_number,".")
